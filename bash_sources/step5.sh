f7a312b8d0d277309284abc4de5193ad2cae7357
ARayane24 <at.rayane03@gmail.com>
2024-08-26 19:08:25 +0100

    add all LFS pkges

--- bash_sources/step5.sh
+++ bash_sources/step5.sh
@@ -2463,7 +2463,8 @@ if [ -n "$OP_MarkupSafe" ] ;then
     tar -xf $OP_MarkupSafe.tar.xz
     cd $OP_MarkupSafe
 
-    
+    pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
+    pip3 install --no-index --no-user --find-links dist Markupsafe
 
     cd /sources/
     rm -Rf $OP_MarkupSafe #rm extracted pkg
@@ -2474,6 +2475,307 @@ fi
 
 
 
+###OP_Jinja:
+if [ -n "$OP_Jinja" ] ;then
+    echo -e "$START_JOB"
+    echo $OP_Jinja
+    tar -xf $OP_Jinja.tar.xz
+    cd $OP_Jinja
+
+    pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
+    pip3 install --no-index --no-user --find-links dist Jinja2
+
+    cd /sources/
+    rm -Rf $OP_Jinja #rm extracted pkg
+    echo -e "$DONE" 
+    echo -e $OP_Jinja "$TOOL_READY"
+fi
+###********************************
+
+
+
+###OP_Udev:
+if [ -n "$OP_Udev" ] ;then
+    echo -e "$START_JOB"
+    echo $OP_Udev
+    tar -xf $OP_Udev.tar.xz
+    cd $OP_Udev
+
+    sed -i -e 's/GROUP="render"/GROUP="video"/' \
+       -e 's/GROUP="sgx", //' rules.d/50-udev-default.rules.in
+
+    sed '/systemd-sysctl/s/^/#/' -i rules.d/99-systemd.rules.in
+    sed '/NETWORK_DIRS/s/systemd/udev/' -i src/basic/path-lookup.h
+
+    mkdir -p build
+    cd       build
+
+    meson setup \
+        --prefix=/usr                 \
+        --buildtype=release           \
+        -Dmode=release                \
+        -Ddev-kvm-mode=0660           \
+        -Dlink-udev-shared=false      \
+        -Dlogind=false                \
+        -Dvconsole=false              \
+        ..
+
+    export udev_helpers=$(grep "'name' :" ../src/udev/meson.build | \
+                      awk '{print $3}' | tr -d ",'" | grep -v 'udevadm')
+    
+    ninja udevadm systemd-hwdb                                           \
+      $(ninja -n | grep -Eo '(src/(lib)?udev|rules.d|hwdb.d)/[^ ]*') \
+      $(realpath libudev.so --relative-to .)                         \
+      $udev_helpers
+
+    install -vm755 -d {/usr/lib,/etc}/udev/{hwdb.d,rules.d,network}
+    install -vm755 -d /usr/{lib,share}/pkgconfig
+    install -vm755 udevadm                             /usr/bin/
+    install -vm755 systemd-hwdb                        /usr/bin/udev-hwdb
+    ln      -svfn  ../bin/udevadm                      /usr/sbin/udevd
+    cp      -av    libudev.so{,*[0-9]}                 /usr/lib/
+    install -vm644 ../src/libudev/libudev.h            /usr/include/
+    install -vm644 src/libudev/*.pc                    /usr/lib/pkgconfig/
+    install -vm644 src/udev/*.pc                       /usr/share/pkgconfig/
+    install -vm644 ../src/udev/udev.conf               /etc/udev/
+    install -vm644 rules.d/* ../rules.d/README         /usr/lib/udev/rules.d/
+    install -vm644 $(find ../rules.d/*.rules \
+                        -not -name '*power-switch*') /usr/lib/udev/rules.d/
+    install -vm644 hwdb.d/*  ../hwdb.d/{*.hwdb,README} /usr/lib/udev/hwdb.d/
+    install -vm755 $udev_helpers                       /usr/lib/udev
+    install -vm644 ../network/99-default.link          /usr/lib/udev/network
+
+    tar -xvf ../../udev-lfs-20230818.tar.xz
+    make -f udev-lfs-20230818/Makefile.lfs install
+
+
+    tar -xf ../../systemd-man-pages-255.tar.xz                            \
+    --no-same-owner --strip-components=1                              \
+    -C /usr/share/man --wildcards '*/udev*' '*/libudev*'              \
+                                  '*/systemd.link.5'                  \
+                                  '*/systemd-'{hwdb,udevd.service}.8
+
+    sed 's|systemd/network|udev/network|'                                 \
+        /usr/share/man/man5/systemd.link.5                                \
+    > /usr/share/man/man5/udev.link.5
+
+    sed 's/systemd\(\\\?-\)/udev\1/' /usr/share/man/man8/systemd-hwdb.8   \
+                                > /usr/share/man/man8/udev-hwdb.8
+
+    sed 's|lib.*udevd|sbin/udevd|'                                        \
+        /usr/share/man/man8/systemd-udevd.service.8                       \
+    > /usr/share/man/man8/udevd.8
+
+    rm /usr/share/man/man*/systemd*
+
+    unset udev_helpers
+    udev-hwdb update
+
+    cd /sources/
+    rm -Rf $OP_Udev #rm extracted pkg
+    echo -e "$DONE" 
+    echo -e $OP_Udev "$TOOL_READY"
+fi
+###********************************
+
+
+
+###OP_Man_DB:
+if [ -n "$OP_Man_DB" ] ;then
+    echo -e "$START_JOB"
+    echo $OP_Man_DB
+    tar -xf $OP_Man_DB.tar.xz
+    cd $OP_Man_DB
+
+    ./configure --prefix=/usr                         \
+            --docdir=/usr/share/doc/man-db-2.12.0 \
+            --sysconfdir=/etc                     \
+            --disable-setuid                      \
+            --enable-cache-owner=bin              \
+            --with-browser=/usr/bin/lynx          \
+            --with-vgrind=/usr/bin/vgrind         \
+            --with-grap=/usr/bin/grap             \
+            --with-systemdtmpfilesdir=            \
+            --with-systemdsystemunitdir=
+    make -s && make -s check && make -s install
+    if [ $? -ne 0 ]; then
+        echo -e "$BUILD_FAILED"
+        exit 1
+    fi
+    echo -e "$BUILD_SUCCEEDED"
+
+    cd /sources/
+    rm -Rf $OP_Man_DB #rm extracted pkg
+    echo -e "$DONE" 
+    echo -e $OP_Man_DB "$TOOL_READY"
+fi
+###********************************
+
+
+
+###OP_Procps_ng:
+if [ -n "$OP_Procps_ng" ] ;then
+    echo -e "$START_JOB"
+    echo $OP_Procps_ng
+    tar -xf $OP_Procps_ng.tar.xz
+    cd $OP_Procps_ng
+
+    if $STATIC_ONLY;then
+        ./configure --prefix=/usr                           \
+            --docdir=/usr/share/doc/$OP_Procps_ng\
+            --enable-static \
+            --disable-shared \
+            --disable-kill
+    else
+        ./configure --prefix=/usr                           \
+            --docdir=/usr/share/doc/$OP_Procps_ng \
+            --disable-static                        \
+            --disable-kill
+    fi
+    
+    make -s && make -ks check && make -s install
+    if [ $? -ne 0 ]; then
+        echo -e "$BUILD_FAILED"
+        exit 1
+    fi
+    echo -e "$BUILD_SUCCEEDED"
+
+    cd /sources/
+    rm -Rf $OP_Procps_ng #rm extracted pkg
+    echo -e "$DONE" 
+    echo -e $OP_Procps_ng "$TOOL_READY"
+fi
+###********************************
+
+
+
+###OP_Util_linux:
+if [ -n "$OP_Util_linux" ] ;then
+    echo -e "$START_JOB"
+    echo $OP_Util_linux
+    tar -xf $OP_Util_linux.tar.xz
+    cd $OP_Util_linux
+
+    sed -i '/test_mkfds/s/^/#/' tests/helpers/Makemodule.am
+
+    if $STATIC_ONLY;then
+        ./configure --bindir=/usr/bin    \
+            --libdir=/usr/lib    \
+            --runstatedir=/run   \
+            --sbindir=/usr/sbin  \
+            --disable-chfn-chsh  \
+            --disable-login      \
+            --disable-nologin    \
+            --disable-su         \
+            --disable-setpriv    \
+            --disable-runuser    \
+            --disable-pylibmount \
+            --enable-static \
+            --disable-shared \
+            --without-python     \
+            --without-systemd    \
+            --without-systemdsystemunitdir        \
+            ADJTIME_PATH=/var/lib/hwclock/adjtime \
+            --docdir=/usr/share/doc/$OP_Util_linux
+    else
+        ./configure --bindir=/usr/bin    \
+            --libdir=/usr/lib    \
+            --runstatedir=/run   \
+            --sbindir=/usr/sbin  \
+            --disable-chfn-chsh  \
+            --disable-login      \
+            --disable-nologin    \
+            --disable-su         \
+            --disable-setpriv    \
+            --disable-runuser    \
+            --disable-pylibmount \
+            --disable-static     \
+            --without-python     \
+            --without-systemd    \
+            --without-systemdsystemunitdir        \
+            ADJTIME_PATH=/var/lib/hwclock/adjtime \
+            --docdir=/usr/share/doc/$OP_Util_linux
+    fi
+    
+    make -s && make -s install
+    if [ $? -ne 0 ]; then
+        echo -e "$BUILD_FAILED"
+        exit 1
+    fi
+    echo -e "$BUILD_SUCCEEDED"
+
+    cd /sources/
+    rm -Rf $OP_Util_linux #rm extracted pkg
+    echo -e "$DONE" 
+    echo -e $OP_Util_linux "$TOOL_READY"
+fi
+###********************************
+
+
+
+###OP_E2fsprogs:
+if [ -n "$OP_E2fsprogs" ] ;then
+    echo -e "$START_JOB"
+    echo $OP_E2fsprogs
+    tar -xf $OP_E2fsprogs.tar.xz
+    cd $OP_E2fsprogs
+
+    mkdir -v build
+    cd       build
+   ../configure --prefix=/usr           \
+             --sysconfdir=/etc       \
+             --enable-elf-shlibs     \
+             --disable-libblkid      \
+             --disable-libuuid       \
+             --disable-uuidd         \
+             --disable-fsck
+    
+    make -s && make -s check && make -s install
+    if [ $? -ne 0 ]; then
+        echo -e "$BUILD_FAILED"
+        exit 1
+    fi
+    echo -e "$BUILD_SUCCEEDED"
+
+    rm -fv /usr/lib/{libcom_err,libe2p,libext2fs,libss}.a
+    gunzip -v /usr/share/info/libext2fs.info.gz
+    install-info --dir-file=/usr/share/info/dir /usr/share/info/libext2fs.info
+
+
+    makeinfo -o      doc/com_err.info ../lib/et/com_err.texinfo
+    install -v -m644 doc/com_err.info /usr/share/info
+    install-info --dir-file=/usr/share/info/dir /usr/share/info/com_err.info
+
+    cd /sources/
+    rm -Rf $OP_E2fsprogs #rm extracted pkg
+    echo -e "$DONE" 
+    echo -e $OP_E2fsprogs "$TOOL_READY"
+fi
+###********************************
+
+
+
+###OP_Sysklogd:
+if [ -n "$OP_Sysklogd" ] ;then
+    echo -e "$START_JOB"
+    echo $OP_Sysklogd
+    tar -xf $OP_Sysklogd.tar.xz
+    cd $OP_Sysklogd
+
+    patch -Np1 -i ../$OP_Sysklogd-consolidated-1.patch
+    make -s && make -s install
+     if [ $? -ne 0 ]; then
+        echo -e "$BUILD_FAILED"
+        exit 1
+    fi
+    echo -e "$BUILD_SUCCEEDED"
+
+    cd /sources/
+    rm -Rf $OP_Sysklogd #rm extracted pkg
+    echo -e "$DONE" 
+    echo -e $OP_Sysklogd "$TOOL_READY"
+fi
+###********************************
 
 
 
