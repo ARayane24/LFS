c824cfa9e549b92afdf4f55c7f31bf227affaba8
ARayane24 <at.rayane03@gmail.com>
2024-08-28 19:16:26 +0100

    fix

--- bash_sources/step7.sh
+++ bash_sources/step7.sh
@@ -10,7 +10,7 @@
 
 rm -rf /tmp/*
 find /usr/lib /usr/libexec -name \*.la -delete
-find /usr -depth -name $(uname -m)-lfs-linux-gnu\* | xargs rm -rf
+find /usr -depth -name ${CPU_SELECTED_ARCH}-${DISTRO_NAME}-linux-gnu\* | xargs rm -rf
 userdel -r tester
 
  echo -e "${STEP}
