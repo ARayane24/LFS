bab64ab21d9b0f463fdfdbc912773a6adebb135f
ARayane24 <at.rayane03@gmail.com>
2024-08-29 12:50:36 +0100

    replace source by bash

--- bash_sources/terminal_params/_config.sh
+++ bash_sources/terminal_params/_config.sh
@@ -3,11 +3,11 @@
 # this bash code was made by ATOUI Rayane to automate the operation of creating Linux from scratch with the help of LFS book v12 (https://www.linuxfromscratch.org/lfs)
 # don't edit this file to insure that it works properly unless you know what are you doing
 
-source ./_colors_in_consol.sh
+bash ./_colors_in_consol.sh
 cd ./languages
 # select_lang found in ./_methodes
 source ./_methodes.sh
-source ./_languages_man.sh
+bash ./_languages_man.sh
 source $SHARED_FILE
 cd ..
 
