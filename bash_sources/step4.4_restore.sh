c8074b2f7e1684aae1e08adb5a7af3553e1f6df0
ARayane24 <at.rayane03@gmail.com>
2024-08-31 00:31:17 +0100

    optimize update

--- bash_sources/step4.4_restore.sh
+++ bash_sources/step4.4_restore.sh
@@ -14,5 +14,6 @@ rm -rf ./*
 tar -xpf $HOME/${DISTRO_NAME}-temp-tools.tar.xz
 cp $HELPER_DIR $LFS
 echo -e "$DONE"
+cp $HELPER_DIR $LFS
 cd $LFS/LFS/bash_sources || cd /LFS/bash_sources 
 bash ./step3.sh
