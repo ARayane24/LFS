d5b5ed105239ef494d8ea8e1991ef4430a9d05ae
ARayane24 <at.rayane03@gmail.com>
2024-10-02 18:19:06 +0100

    fix blfs downloads

--- BLFS/download.sh
+++ BLFS/download.sh
@@ -3,7 +3,7 @@
 # this bash code was made by chroot team to automate the operation of creating Linux from scratch with the help of LFS book v12.2 (https://www.linuxfromscratch.org/lfs)
 # don't edit this file to insure that it works properly unless you know what are you doing
 
-
+source ../bash_sources/terminal_params/_util_methodes.sh
 
 CODE_SOURCES_INSTALLED=$(yes_no_question "$DO_YOU_HAVE_CODE_SOURCES")
 
@@ -15,8 +15,53 @@ else
     downlaod_utils_pkgs
 
     echo -e "$START_DOWNLOAD_CODE_SOURCES"
-    wget -nv --input-file=./all-blfs-wget-list --continue --directory-prefix=$LFS/sources/blfs
+    # Directory to check for existing packages
+    download_dir="$HELPER_DIR/sources/blfs"
+    source_file="./all-blfs-wget-list"
+    elinks="./error_links"
 
-    chown root:root $LFS/sources/blfs/*
-    echo -e "$DONE"
+    # Create the download directory if it doesn't exist
+    mkdir -p "$download_dir"
+
+    # Read each URL from the file
+    while IFS= read -r url; do
+        package_name=$(basename "$url")  # No need for 'export'
+
+        if [[ -f "$download_dir/$package_name" ]]; then
+            echo "Package $package_name already exists."
+        else
+            wget -nv --directory-prefix="$download_dir" "$url"  || echo "$url" >> $elinks
+            echo "Downloaded $package_name"
+        fi
+    done < $source_file
+
+    while IFS= read -r url; do
+        package_name=$(basename "$url")  # No need for 'export'
+
+        if [[ -f "$download_dir/$package_name" ]]; then
+            p=1
+        else
+            lynx -dump -listonly -nonumbers "$url"  > "links.txt"
+            while IFS= read -r url; do
+                package_name=$(basename "$url")  # No need for 'export'
+
+                if [[ -f "$download_dir/$package_name" ]]; then
+                    p=1
+                else
+                    wget -nv --directory-prefix="$download_dir" "$url"  || echo "$url" >> $elinks-no-solution
+                    echo "Downloaded $package_name"
+                fi
+            done < "links.txt"
+        fi
+    done < $elinks
 fi
+
+
+cp -Rf $HELPER_DIR/sources/blfs $LFS/sources
+chown root:root $LFS/sources/blfs
+chown root:root $LFS/sources/blfs/*
+echo -e "$DONE"
+
+
+
+
